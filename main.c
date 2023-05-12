03a026c57374a92a6b6c648b0017a88a7720d9be
McMugah <blainelium@gmail.com>
2023-05-12 17:52:35 +0300

    shell project

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Main - a shell to interpret command
+ *
+ *
+ *Return: 0 Always
+ */
+int main(){
+char command[100];
+while (1){
+printf("$ ");
+fgets(command, sizeof(command), stdin);
+printf("%s", command);
+}
+return 0;
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+#endif
+
