5688c6d782c4b8ab86f55ab1e44dc25ca9b076fa
intexted <lotfiaouane@gmail.com>
2023-05-16 05:23:38 -0700

    first task

--- main.c
+++ main.c
@@ -5,25 +5,55 @@
  *
  *Return: 0 Always
  */
+
+
 int main(void)
 {
 	char *command = NULL;
 	size_t bufsize = 0;
+	ssize_t chars_read;
+	char *token;
+	char *args[COMMAND_LENGTH];
+	pid_t pid;
 
 	while (1)
-	{	
+	{
 		printf("$ ");
-	       	ssize_t chars_read;
 		chars_read = getline(&command, &bufsize, stdin);
-		if (chars_read == -1) {
-			if (feof(stdin)) {
-			       	printf("\n");
+		if (chars_read == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
 				break;
-			} else {
-				perror("error");
 			}
-		}	
-		printf("%s", command);
+		}
+
+		command[strcspn(command, "\n")] = '\0';
+
+		token = strtok(command, " ");
+		if (token != NULL)
+		{
+			args[0] = token;
+			args[1] = NULL;
+		} else
+		{
+			continue;
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+			execvp(args[0], args);
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		} else if (pid > 0)
+		{
+			wait(NULL);
+		} else
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
 	}
 	return (0);
 }
--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-
+#define COMMAND_LENGTH 100
 
 #endif
 
