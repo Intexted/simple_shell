044a346ce022c84e87a94a69d50ddf0f30eb82d1
intexted <lotfiaouane@gmail.com>
2023-05-22 05:21:53 -0700

    add some requirement

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * main - a shell to interpret command
  *
  *
  *Return: 0 Always
  */
-
 
 int main(void)
 {
@@ -14,6 +14,7 @@ int main(void)
 	ssize_t chars_read;
 	char *token;
 	char *args[COMMAND_LENGTH];
+	int i;
 	pid_t pid;
 
 	while (1)
@@ -27,18 +28,31 @@ int main(void)
 				printf("\n");
 				break;
 			}
+			else if (ferror(stdin))
+			{
+				perror("getline");
+				exit(EXIT_FAILURE);
+			}
 		}
 
 		command[strcspn(command, "\n")] = '\0';
-
 		token = strtok(command, " ");
 		if (token != NULL)
 		{
 			args[0] = token;
-			args[1] = NULL;
+			i = 1;
 		} else
 		{
 			continue;
+		}
+		while ((token = strtok(NULL, " ")) != NULL)
+		{
+			args[i++] = token;
+		}
+		args[i] = NULL;
+		if (strcmp(args[0], "exit") == 0)
+		{
+			break;
 		}
 		pid = fork();
 		if (pid == 0)
@@ -55,5 +69,7 @@ int main(void)
 			exit(EXIT_FAILURE);
 		}
 	}
+	free(command);
 	return (0);
 }
+
